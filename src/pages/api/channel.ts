5ffa31e3947529e5835cf4bfdb56a384b7f01356
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-11-25 22:38:48 -0500

    Update channel.ts

--- src/pages/api/channel.ts
+++ src/pages/api/channel.ts
@@ -1,18 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import chromium from 'chrome-aws-lambda'; // Import chrome-aws-lambda
-import { KickApiWrapper } from 'kick.com-api'; // Your custom module
+import chromium from 'chrome-aws-lambda';
+import { KickApiWrapper } from 'kick.com-api';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // Configure KickApiWrapper for serverless environment
     const kickApi = new KickApiWrapper({
       apiVersion: 'v1',
-      puppeteer: {
-        executablePath: await chromium.executablePath,
-      },
     });
 
-    // Fetch channel data using the specified API version
     const channelData = await kickApi.fetchChannelData(req.query.channelName as string, 'v1');
     res.status(200).json(channelData);
   } catch (error) {
